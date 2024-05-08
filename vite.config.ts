8c7f2dd7bf2de36c50c39b5fd168c22e30f3fbda
filatov <filatov2@finolab.ru>
2024-05-08 12:50:45 +0300

    feat: fix shop path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/carusel_gp_site/',
+  base: '/shop/',
   plugins: [react(), tsconfigPaths()],
 });
